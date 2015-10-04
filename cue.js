13404f9b71403989b594b92886c163c81e047bcf
Jim Lemmers <jimlemmers@Macintosh.local>
2015-10-04 18:43:35 +0200

    refactored the code to have a create function and export function

--- cue.js
+++ cue.js
@@ -6,27 +6,70 @@
  * 2. Inspecteren om een Object Inspector op het resultaat te tonen (Cmd-I), of
  * 3. Weergeven om het resultaat in een opmerking na de selectie in te voeren. (Cmd-L)
  */
+function main(){
+  var description = document.getElementById('eow-description');
+  var links = description.getElementsByTagName('a');
+  var current_video = document.URL;
+  var title = document.title.split('-');
+  var artist = title[0].trim();
 
-var description = document.getElementById('eow-description');
-var links = description.getElementsByTagName('a');
-var current_video = document.URL;
-var title = document.title.split('-');
-var artist = title[0].trim();
-console.log(current_video);
-var cue = 'PERFORMER ' + artist + '\n' +
-           'TITLE ' + title[1].replace(' ', '') + '\n' +
-           'FILE ' + '' + '\n';
+  var cue = {'performer': artist,      
+             'title': title[1].replace(' ', ''),
+             'file' : '',
+             'tracks' : [],
+            };
 
-for (i = 0; i < links.length ; i++) {
-  if (links[i].href == current_video + '#') {
-    var index = i + 1;
-    var name = links[i].nextSibling.data;
-    name = name.trim().replace(/\d*/,'').trim();
-    var time = links[i].childNodes[0].data;
-    cue = cue.concat('\t TRACK O' + index + ' AUDIO \n');
-    cue = cue.concat('\t\t TITLE "' + name + '"\n');
-    cue = cue.concat('\t\t PERFORMER "' + artist +'"\n');
-    cue = cue.concat('\t\t INDEX 01 ' + time + ':00 \n');
+  for (i = 0; i < links.length ; i++) {
+    if (links[i].href == current_video + '#') {
+      var name, index;
+      index = i + 1;
+      if (links[i].nextSibling !== null && links[i].nextSibling.data){
+        name = links[i].nextSibling.data;
+      }else {
+        console.log('previous');
+        name = links[i].previousSibling.data;
+      }
+      name = name.trim().replace(/[\d*\"]/g,'').trim();
+      var time = links[i].childNodes[0].data;
+      cue.tracks.push({'index': index,
+                       'title': name,
+                       'performer': artist,
+                       'time': time + ':00'})
+
+    }
   }
+  return cue;
 }
+
+function create_cue_text(cue){
+  cue_text = 'PERFORMER ' + cue.performer + ' \n' +
+             'TITLE ' + cue.title + ' \n' +
+             'FILE ' + cue.file + '\n';
+  for (i=0; i<cue.tracks.length; i++){
+    var track = cue.tracks[i];
+    cue_text = cue_text.concat('\t TRACK ' + track.index + ' AUDIO \n');
+    cue_text = cue_text.concat('\t\t TITLE "' + track.title + '"\n');
+    cue_text = cue_text.concat('\t\t PERFORMER "' + track.performer +'"\n');
+    cue_text = cue_text.concat('\t\t INDEX 01 ' + track.time + ':00 \n');
+  }
+  return cue_text;
+}
+var cue = main();
+var cue_text = create_cue_text(cue);
+console.log(cue_text);
+var link = document.createElement('a');
+link.href = 'data:text/plain;charset=UTF-8,' + encodeURIComponent(cue_text);
+link.innerHTML = 'Download the cue file here' ;
+description.appendChild(link);
 console.log(cue);
+
+/*
+Exception: TypeError: cue.TRACKS is undefined
+main@Scratchpad/1:34:7
+@Scratchpad/1:57:11
+*/
+/*
+Exception: TypeError: cue.concat is not a function
+create_cue_text@Scratchpad/1:50:16
+@Scratchpad/1:58:16
+*/
